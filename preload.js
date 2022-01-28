b25fdeb2ff4cadfe9c93d9c9ea8282dcd9efb066
gattotozzo <daniel282016@icloud.com>
2022-01-28 19:26:41 +0100

    adding big-display

--- preload.js
+++ preload.js
@@ -1,12 +1,4 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const Stopwatch = require("statman-stopwatch");
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+window.puttana = process.versions;
+window.Stopwatch = Stopwatch;
--- renderer.js
+++ renderer.js
@@ -4,3 +4,23 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function msToTime(duration) {
+    let milliseconds = Math.floor((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60);
+  
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    return minutes + ":" + seconds + ":" + milliseconds;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    let display = document.getElementById("big-display");
+    let stopwatch = new Stopwatch();
+    setInterval(() => {
+        display.innerText = msToTime(stopwatch.read());
+    }, 10);
+    stopwatch.start();
+});
